40a4ef82d81a11b55dfef82415ab5b633875b9ad
Amber Malpas <ambermalpas@gmail.com>
2025-06-03 18:48:30 -0400

    Add docstrings to magnification functions

--- Event/_VBM.py
+++ Event/_VBM.py
@@ -2,6 +2,30 @@ import numpy as np
 import VBMicrolensing
 
 def magnification(self, ss, q, u1, u2, rho, eps=1e-4, gamma=0.36):
+    """Return the binary-lens magnification using :mod:`VBMicrolensing`.
+
+    Parameters
+    ----------
+    ss : array_like
+        Lens separation for each epoch in units of ``theta_E``.
+    q : float
+        Mass ratio ``m_2/m_1`` of the lens system.
+    u1 : array_like
+        Source position relative to the primary lens for each epoch.
+    u2 : array_like
+        Source position relative to the secondary lens for each epoch.
+    rho : float
+        Angular radius of the source in units of ``theta_E``.
+    eps : float, optional
+        Relative tolerance passed to :mod:`VBMicrolensing`. Default ``1e-4``.
+    gamma : float, optional
+        Limb-darkening coefficient ``a1`` for :mod:`VBMicrolensing`.
+
+    Returns
+    -------
+    ndarray
+        Magnification for each value of ``ss``.
+    """
 
 	if self.mag_obj is None:
 		self.mag_obj = VBMicrolensing.VBMicrolensing()
@@ -16,4 +40,4 @@ def magnification(self, ss, q, u1, u2, rho, eps=1e-4, gamma=0.36):
 		#print(ss[i], q, u1[i], u2[i], rho)
 		mag[i] = self.mag_obj.BinaryMag2(ss[i], q, u1[i], u2[i], rho)
 
-	return np.array(mag)
+        return np.array(mag)
